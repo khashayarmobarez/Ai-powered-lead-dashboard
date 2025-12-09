f9576d8918db1c3974178333d8e8068d117fb7fc
khashayar <khashayarmobarezbusiness@gmail.com>
2025-12-09 18:04:45 +0330

    testing the daisyui and global css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div >
-      
+      <button className="btn btn-primary">Prime Target</button>
+      <h1 className="shine">Lead Forge</h1>
     </div>
   );
 }
